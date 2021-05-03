006899ad8b68b16ae0366e213e00c715c120a159
Scott Davies <scottdavies1983@gmail.com>
2021-05-03 14:34:43 +0100

    setup of header icons

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import Header from "./components/Header";
 import SearchTitles from "./components/SearchTitles";
 import Selections from "./components/Selections";
@@ -10,7 +11,6 @@ function Search(props) {
   return (
     <div className="search-div">
       <Header config={{ button1: "login", button2: "user" }} />
-      <h1>THIS IS THE SEARCH PAGE</h1>
       <Selections />
       <SearchTitles />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,33 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { Link } from "react-router-dom";
+import { HiEye } from "react-icons/hi";
+import { IoMdUnlock as Lock } from "react-icons/io";
+import { BsSearch } from "react-icons/bs";
+import { FaUserAlt } from "react-icons/fa";
 
 function Header(props) {
   const { button1, button2 } = props.config;
+
   return (
     <div className="header">
-      <h5>THIS IS THE HEADER</h5>
-      <Link to={`/${button1}`}>
-        <button type="button">{button1}</button>
-      </Link>
-      <Link to={`/${button2}`}>
-        <button type="button">{button2}</button>
-      </Link>
+      <h1 className="app-title">CUSHION ROULETTE</h1>
+      <div className="header-buttons">
+        <Link to={`/${button1}`}>
+          <button className="header-button" type="button">
+            {button1 === "login" && <Lock />}
+            {button1 === "search" && <BsSearch />}
+            {button1 === "user" && <FaUserAlt />}
+          </button>
+        </Link>
+        <Link to={`/${button2}`}>
+          <button className="header-button" type="button">
+            {button2 === "login" && <Lock />}
+            {button2 === "search" && <BsSearch />}
+            {button2 === "user" && <FaUserAlt />}
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
