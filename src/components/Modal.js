46d69675d8cc0bd1d7c3b137885588e91e6a3fac
Scott Davies <scottdavies1983@gmail.com>
2021-06-16 09:55:21 +0100

    started work on modal window

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useEffect } from "react";
 
 // Proptypes ?
 // Move this conditional rendering up one level to Component Render
@@ -7,9 +7,39 @@ import React from "react";
 function Modal(props) {
   const { visibility, programmeNumber } = props;
 
+  // Is there a better way in react of not using querySelector?
+  // Investigate use ref
+
+  useEffect(() => {
+    const modalOuter = document.querySelector(".modal-outer");
+
+    function closeModal() {
+      modalOuter.classList.remove("open");
+    }
+
+    modalOuter.addEventListener("click", function (event) {
+      // the ! changes this to a boolean , if it finds the .modal-inner it will be false
+      const isOutside = !event.target.closest(".modal-inner");
+      if (isOutside) {
+        closeModal();
+      }
+    });
+  });
+
   return (
     <div>
-      <h1>{visibility ? `You have selected ${programmeNumber}` : null}</h1>
+      <div className={visibility ? "modal-outer open" : "modal-outer"}>
+        <div className={visibility ? "modal-inner open" : "modal-inner"}>
+          <p>You have selected!!</p>
+          <img
+            width="600"
+            height="600"
+            // src={imgSrc.replace("200", "600")}
+            alt={programmeNumber}
+          />
+          <p>{programmeNumber}</p>
+        </div>
+      </div>
     </div>
   );
 }
