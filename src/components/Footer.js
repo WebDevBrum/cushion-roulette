ece91ff8932f77825f3821af7fc8227e321fe8a8
Scott Davies <scottdavies1983@gmail.com>
2021-07-25 20:03:36 +0100

    added api attribution

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,15 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import apiLogo from "../images/tmdb.png";
 
 function Footer(props) {
   const { clear, buttonText } = props;
   return (
     <div className="footer">
+      <img className="api-logo" src={apiLogo} alt="api logo" />
+      <p className="api-attribution">
+        This product uses the TMDb API but is not endorsed or certified by TMDb.
+      </p>
       <div className="footer-buttons">
         <button className="clear-button" type="button" onClick={() => clear()}>
           {buttonText}
